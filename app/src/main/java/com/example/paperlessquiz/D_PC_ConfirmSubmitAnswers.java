f10894d8050c4f23b5f890908cab17da7748f858
Rupert Mergan <rupert_mergan@yahoo.com>
2019-05-10 20:01:30 +0200

    Added Subit button, but this is actually the Cancel ;-)

--- app/src/main/java/com/example/paperlessquiz/D_PC_ConfirmSubmitAnswers.java
+++ app/src/main/java/com/example/paperlessquiz/D_PC_ConfirmSubmitAnswers.java
@@ -1,12 +1,16 @@
 package com.example.paperlessquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.paperlessquiz.adapters.ReviewAnswersAdapter;
 import com.example.paperlessquiz.answer.Answer;
+import com.example.paperlessquiz.google.access.GoogleAccess;
 import com.example.paperlessquiz.loginentity.LoginEntity;
 import com.example.paperlessquiz.question.QuestionsList;
 import com.example.paperlessquiz.quizbasics.QuizBasics;
@@ -21,9 +25,8 @@ public class D_PC_ConfirmSubmitAnswers extends AppCompatActivity {
     QuizBasics thisQuizBasics;
     LoginEntity thisLoginEntity;
     ArrayList<Answer> answers;
-    TextView tvDisplayAnswers;
-    String displayAnswers = "";
     ListView lvAnswers;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +35,29 @@ public class D_PC_ConfirmSubmitAnswers extends AppCompatActivity {
         thisQuizBasics = (QuizBasics)getIntent().getSerializableExtra(QuizBasics.INTENT_EXTRA_NAME_THIS_QUIZ_BASICS);
         thisQuizExtras = (QuizExtras)getIntent().getSerializableExtra(QuizExtras.INTENT_EXTRA_NAME_THIS_QUIZ_EXTRAS);
         thisLoginEntity = (LoginEntity)getIntent().getSerializableExtra(LoginEntity.INTENT_EXTRA_NAME_THIS_LOGIN_ENTITY);
-
         answers = (ArrayList<Answer>) getIntent().getSerializableExtra(QuestionsList.INTENT_PUT_EXTRA_NAME_THIS_ROUND_ANSWERS);
-        //String answer1 = (String) getIntent().getStringExtra(QuestionsList.INTENT_PUT_EXTRA_NAME_THIS_ROUND_ANSWERS);
-        //tvDisplayAnswers = (TextView)findViewById(R.id.tvDisplayAnswers);
-        //for (int i=0;i<10;i++){
-        //    displayAnswers = displayAnswers + " Question" + i + answers.get(i);
-        //}
-        //tvDisplayAnswers.setText(displayAnswers);
         lvAnswers = (ListView) findViewById(R.id.lv_DisplayAnswers);
+        btnSubmit = (Button) findViewById(R.id.btnSubmitAnswers);
         ReviewAnswersAdapter adapter = new ReviewAnswersAdapter(this,answers);
         lvAnswers.setAdapter(adapter);
+
+        btnSubmit.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        //Intent intent = new Intent(D_PC_ConfirmSubmitAnswers.this, D_PA_ShowRounds.class);
+                        //intent.putExtra(QuizBasics.INTENT_EXTRA_NAME_THIS_QUIZ_BASICS, thisQuizBasics);
+                        //intent.putExtra(QuizExtras.INTENT_EXTRA_NAME_THIS_QUIZ_EXTRAS, thisQuizExtras);
+                        //intent.putExtra(LoginEntity.INTENT_EXTRA_NAME_THIS_LOGIN_ENTITY, thisLoginEntity);
+                        //intent.putExtra(QuestionsList.INTENT_PUT_EXTRA_NAME_THIS_ROUND_ANSWERS,adapter.getMyAnswers());
+                        //GoogleAccessSet submitAnswers = new GoogleAccess();
+                        //startActivity(intent);
+                        finish();
+                    }
+                }
+        );
 
     }
 }
